c4c262e70833c52bafc4979ea4649f6688faf53f
Andrei Markin <amarkin@google.com>
2020-10-06 15:05:43 +0300

    Move all config parameters to config file

--- doorcam.py
+++ doorcam.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 from datetime import datetime, timedelta
 import pickle
@@ -21,30 +22,25 @@ PARSER.set_defaults(show_video=False)
 ARGS = PARSER.parse_args()
 
 logging.basicConfig(
-    filename='doorcam.log',
+    filename='/home/am/projects/opencv_examples/doorcam.log',
     filemode='a',
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level = logging.INFO
 )
 
+# touch file to make sure program is running
+os.system("touch /home/am/projects/opencv_examples/doorcam.txt")
 # Our list of known face encodings and a matching list of metadata about each face.
 known_face_encodings = []
 known_face_metadata = []
 ids = []
 face_hashes = []
 
-client = InfluxDBClient(config.INFLUX_DB_ADDRESS, 8086)
+client = InfluxDBClient(config.INFLUX_DB_ADDRESS, config.INFLUX_DB_PORT)
 client.switch_database(config.INFLUX_TABLE)
 
-def save_known_faces():
-    with open("known_faces.dat", "wb") as face_data_file:
-        face_data = [known_face_encodings, known_face_metadata]
-        pickle.dump(face_data, face_data_file)
-        print("Known faces backed up to disk.")
-        logging.info("Known faces backed up to disk.")
-
 def open_close_connection(f):
-    conn = ps.connect("dbname=face_recognition user=am")
+    conn = ps.connect(f"dbname={config.POSTGRES_DB} user={config.POSTGRES_USER}")
     cur = conn.cursor()
     f()
     cur.close()
@@ -55,7 +51,7 @@ def save_face_pg(face_encoding, face_image):
     def list2pgarray(alist):
         return '{' + ','.join([str(i) for i in alist]) + '}'
 
-    conn = ps.connect("dbname=face_recognition user=am")
+    conn = ps.connect(f"dbname={config.POSTGRES_DB} user={config.POSTGRES_USER}")
     cur = conn.cursor()
 
     face_image = np.reshape(face_image, (67500))
@@ -90,11 +86,7 @@ def load_known_faces_pg():
 
     global known_face_encodings, known_face_metadata, ids, face_hashes 
 
-    conn = ps.connect("dbname=face_recognition user=am")
-    cur = conn.cursor()
-    
-
-    conn = ps.connect("dbname=face_recognition user=am")
+    conn = ps.connect(f"dbname={config.POSTGRES_DB} user={config.POSTGRES_USER}")
     cur = conn.cursor()
     cur.execute("""
         SELECT
@@ -115,20 +107,6 @@ def load_known_faces_pg():
 
     return known_face_encodings
 
-
-def load_known_faces():
-    global known_face_encodings, known_face_metadata
-
-    try:
-        with open("known_faces.dat", "rb") as face_data_file:
-            known_face_encodings, known_face_metadata = pickle.load(face_data_file)
-            print("Known faces loaded from disk.")
-            logging.info("Known faces loaded from disk.")
-    except FileNotFoundError as e:
-        print("No previous face data found - starting with a blank known face list.")
-        pass
-
-
 def running_on_jetson_nano():
     # To make the same code work on a laptop or on a Jetson Nano, we'll detect when we are running on the Nano
     # so that we can access the camera correctly in that case.
@@ -148,25 +126,6 @@ def get_jetson_gstreamer_source(capture_width=1280, capture_height=720, display_
             f'video/x-raw, width=(int){display_width}, height=(int){display_height}, format=(string)BGRx ! ' +
             'videoconvert ! video/x-raw, format=(string)BGR ! appsink'
             )
-
-
-def register_new_face(face_encoding, face_image):
-    """
-    Add a new person to our list of known faces
-    """
-    # Add the face encoding to the list of known faces
-    known_face_encodings.append(face_encoding)
-    # Add a matching dictionary entry to our metadata list.
-    # We can use this to keep track of how many times a person has visited, when we last saw them, etc.
-    known_face_metadata.append({
-        "first_seen": datetime.now(),
-        "first_seen_this_interaction": datetime.now(),
-        "last_seen": datetime.now(),
-        "seen_count": 1,
-        "seen_frames": 1,
-        "face_image": face_image,
-    })
-
 
 def lookup_known_face_pg(face_encoding):
 
@@ -194,46 +153,6 @@ def write_face_info_to_influx(face_id, client):
     ]
     client.write_points(json_body)
 
-def lookup_known_face(face_encoding):
-    """
-    See if this is a face we already have in our face list
-    """
-    metadata = None
-
-    # If our known face list is empty, just return nothing since we can't possibly have seen this face.
-    if len(known_face_encodings) == 0:
-        return metadata
-
-    # Calculate the face distance between the unknown face and every face on in our known face list
-    # This will return a floating point number between 0.0 and 1.0 for each known face. The smaller the number,
-    # the more similar that face was to the unknown face.
-    face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-
-    # Get the known face that had the lowest distance (i.e. most similar) from the unknown face.
-    best_match_index = np.argmin(face_distances)
-
-    # If the face with the lowest distance had a distance under 0.6, we consider it a face match.
-    # 0.6 comes from how the face recognition model was trained. It was trained to make sure pictures
-    # of the same person always were less than 0.6 away from each other.
-    # Here, we are loosening the threshold a little bit to 0.65 because it is unlikely that two very similar
-    # people will come up to the door at the same time.
-    if face_distances[best_match_index] < 0.65:
-        # If we have a match, look up the metadata we've saved for it (like the first time we saw it, etc)
-        metadata = known_face_metadata[best_match_index]
-
-        # Update the metadata for the face so we can keep track of how recently we have seen this face.
-        metadata["last_seen"] = datetime.now()
-        metadata["seen_frames"] += 1
-
-        # We'll also keep a total "seen count" that tracks how many times this person has come to the door.
-        # But we can say that if we have seen this person within the last 5 minutes, it is still the same
-        # visit, not a new visit. But if they go away for awhile and come back, that is a new visit.
-        if datetime.now() - metadata["first_seen_this_interaction"] > timedelta(minutes=5):
-            metadata["first_seen_this_interaction"] = datetime.now()
-            metadata["seen_count"] += 1
-
-    return metadata
-
 def main_loop(mode="local", stream=None, resize=1.0, show_video=False):
 
     logging.info("starting program")
@@ -253,19 +172,23 @@ def main_loop(mode="local", stream=None, resize=1.0, show_video=False):
     # Track how long since we last saved a copy of our known faces to disk as a backup.
     number_of_faces_since_save = 0
 
+    i = 0
+    index = 0
     while True:
-        index = 0
         # Grab a single frame of video
         ret, frame = video_capture.read()
         index+=1
-        if index > 60:
-            video_capture.release()
-            video_capture = cv2.VideoCapture(stream)
-            logging.warning("Reinitialized the camera")
+        # if index > 60:
+        #     video_capture.release()
+        #     video_capture = cv2.VideoCapture(stream)
+        #     logging.warning("Reinitialized the camera")
 
         # proceed only when OpenCV managed to read the frame
         if ret:
-
+            i+=1
+            if i == 20:
+                os.system("touch /home/am/projects/opencv_examples/doorcam.txt")
+                i = 0
             index-=1
 
             # Resize frame of video to 1/4 size for faster face recognition processing
@@ -358,11 +281,11 @@ def main_loop(mode="local", stream=None, resize=1.0, show_video=False):
                     break
 
             # We need to save our known faces back to disk every so often in case something crashes.
-            if len(face_locations) > 0 and number_of_faces_since_save > 100:
-                save_known_faces()
-                number_of_faces_since_save = 0
-            else:
-                number_of_faces_since_save += 1
+            # if len(face_locations) > 0 and number_of_faces_since_save > 100:
+            #     save_known_faces()
+            #     number_of_faces_since_save = 0
+            # else:
+            #     number_of_faces_since_save += 1
 
     # Release handle to the webcam
     video_capture.release()
